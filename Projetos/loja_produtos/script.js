951367313508787f1d3617900c879191a10d7269
minoru-yamanaka <minoruyamanaka@icloud.com>
2025-01-23 20:11:27 -0300

    230125-2011

--- Projetos/loja_produtos/script.js
+++ Projetos/loja_produtos/script.js
@@ -1,21 +1,37 @@
-// Seleciona todos os elementos com a classe 'card' e armazena-os na variável 'cards'
 const cards = document.querySelectorAll('.card');
-
-// Seleciona o botão de redefinir pelo seu ID e armazena-o na variável 'btnRedefinir'
 const btnRedefinir = document.getElementById('btnRedefinir');
+const btnComprar = document.getElementById('btnComprar');
+const resultado = document.getElementById('resultado');
 
-// Para cada elemento 'card', adiciona um ouvinte de evento para o clique
 cards.forEach(card => {
-    // Quando um 'card' for clicado, alterna a classe 'selecionado'
     card.addEventListener("click", () => {
-        card.classList.toggle("selecionado"); // Alterna a classe 'selecionado' para destacar ou desmarcar o produto
-    });
-});
+        card.classList.toggle("selecionado")
+    })
+})
 
-// Para cada 'card', adiciona um ouvinte de evento para o botão 'btnRedefinir'
 cards.forEach(card => {
-    // Quando o botão 'btnRedefinir' for clicado, remove a classe 'selecionado' de todos os 'cards'
     btnRedefinir.addEventListener("click", () => {
-        card.classList.remove("selecionado"); // Remove a classe 'selecionado' de cada 'card', desfazendo a seleção
+        card.style.display = "";
+        card.classList.remove("selecionado");
+        resultado.innerText = "";
+    })
+    
+})
+
+// FUNÇÃO DE COMPRAR DEVE
+// 1 - ESCONDER OS CARDS QUE NÃO ESTÃO SELECIONADOS #OK
+// 2 - CALCULAR O PREÇO DOS CARDS VISIVEIS
+// 3 - EXIBIR UM TOTAL DA SOMA DOS CARDS VISIVEIS
+
+btnComprar.addEventListener("click", () => {
+    let precoTotal = 0;
+
+    cards.forEach(card => {
+        if(!card.classList.contains("selecionado")) {
+            card.style.display = "none";
+        } else {
+            precoTotal += parseFloat(card.dataset.preco);
+        }
     });
+    resultado.innerText = precoTotal;
 });
