96ea25dbf87d50ea51ecc2b108dc338412742cbb
minoru-yamanaka <minoruyamanaka@icloud.com>
2025-01-23 21:41:08 -0300

    teste 2301252141

--- Projetos/loja_produtos/script.js
+++ Projetos/loja_produtos/script.js
@@ -2,6 +2,8 @@ const cards = document.querySelectorAll('.card');
 const btnRedefinir = document.getElementById('btnRedefinir');
 const btnComprar = document.getElementById('btnComprar');
 const resultado = document.getElementById('resultado');
+const telefoneDoWhatsapp = "5511959473402";
+const whatsappLink = document.getElementById("whatsappLink");
 
 cards.forEach(card => {
     card.addEventListener("click", () => {
@@ -12,26 +14,29 @@ cards.forEach(card => {
 cards.forEach(card => {
     btnRedefinir.addEventListener("click", () => {
         card.style.display = "";
-        card.classList.remove("selecionado");
+        card.classList.remove("selecionado")
         resultado.innerText = "";
     })
-    
 })
-
-// FUNÇÃO DE COMPRAR DEVE
-// 1 - ESCONDER OS CARDS QUE NÃO ESTÃO SELECIONADOS #OK
-// 2 - CALCULAR O PREÇO DOS CARDS VISIVEIS
-// 3 - EXIBIR UM TOTAL DA SOMA DOS CARDS VISIVEIS
 
 btnComprar.addEventListener("click", () => {
     let precoTotal = 0;
+    let listaProdutos = [];
 
     cards.forEach(card => {
         if(!card.classList.contains("selecionado")) {
             card.style.display = "none";
         } else {
+            listaProdutos.push(
+                card.querySelector("div:first-child").textContent + " R$ $" + card.dataset.preco);
             precoTotal += parseFloat(card.dataset.preco);
         }
     });
-    resultado.innerText = `Preço total R$ ${precoTotal}`;
+    resultado.innerText = `Preço Total R$ ${precoTotal}`;
+
+    const mensagem = encodeURI(`Olá, gostaria de comprar os seguintes produtos:\n\n${listaProdutos.join("\n")}`);
+
+    whatsappLink.href = `https://wa.me/${telefoneDoWhatsapp}?text=${mensagem}`;
+
+
 });
